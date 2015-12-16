48719b013bfe80be5bb8c3deb8e9f41c11ef4e21
Frédéric Chanson <fchanson@users.noreply.github.com>
2015-12-16 14:28:45 +0100

    Create speaker.js

---
+++ speaker/speaker.js
@@ -0,0 +1,41 @@
+
+module.exports = function(RED) {
+    "use strict";
+    var Speaker = require('speaker');
+    var speakerList = {};
+    
+    function SpeakerOut(n) {
+        RED.nodes.createNode(this,n);
+        this.name = n.name;
+        this.channels = n.channels;
+        this.bitDepth = n.bitDepth;
+        this.sampleRate = n.sampleRate;
+        var node = this;
+        var speaker = null;
+
+        
+        node.on("input", function(msg) {
+            if (msg.speech && Buffer.byteLength(msg.speech)>0) {
+            	var params = (msg.channels || this.channels)+'_'+(msg.bitDepth || this.bitDepth)+'_'+(msg.sampleRate || this.sampleRate);
+            	speaker = speakerList[params];
+            	if(!speaker) {
+            		speaker = new Speaker({
+            	          channels: msg.channels || this.channels,          
+            	          bitDepth: msg.bitDepth || this.bitDepth,         
+            	          sampleRate: msg.sampleRate || this.sampleRate
+            	        });
+            		speakerList[params] = speaker;
+            	}
+                speaker.write(msg.speech);
+            }
+        });
+
+        node.on("close", function() {
+            this.closing = true;
+
+        });
+
+    }
+    RED.nodes.registerType("speaker out",SpeakerOut);
+
+}
